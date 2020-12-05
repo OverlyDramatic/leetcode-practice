5a23bd7abfa1265df495a113b77bdd671555a6d5
overlyDramatic <maxjunedown@live.com>
2020-12-05 19:08:36 +0800

    罗马数字转整数 解答

--- src/04-romanToInt.js
+++ src/04-romanToInt.js
@@ -1,6 +1,46 @@
 /**
  * * 罗马数字转整数 https://leetcode-cn.com/problems/roman-to-integer/
+ * I             1
+ * V             5
+ * X             10
+ * L             50
+ * C             100
+ * D             500
+ * M             1000
  * @param {string} s
  * @return {number}
  */
-var romanToInt = function (s) {}
+const romanMap = {
+  I: 1,
+  IV: 4,
+  V: 5,
+  IX: 9,
+  X: 10,
+  XL: 40,
+  L: 50,
+  XC: 90,
+  C: 100,
+  CD: 400,
+  D: 500,
+  CM: 900,
+  M: 1000,
+}
+
+function romanToInt (s) {
+  let result = 0
+  const length = s.length
+  for (let i = 0; i < length; i++) {
+    if (s[i] === 'I' || s[i] === 'X' || s[i] === 'C') {
+      const _num = romanMap[s[i] + s[i + 1]]
+      if (_num) {
+        result += _num
+        ++i
+        continue
+      }
+    }
+    result += romanMap[s[i]]
+  }
+  return result
+}
+
+module.exports = romanToInt
