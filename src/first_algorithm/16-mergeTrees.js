289f3f6dff5868af896063baae671711160fa2c7
overlyDramatic <maxjunedown@live.com>
2022-06-19 15:33:29 +0800

    feat: mergeTrees

---
+++ src/first_algorithm/16-mergeTrees.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * * Merge Two Binary Trees
+ * https://leetcode.cn/problems/merge-two-binary-trees/
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {TreeNode}
+ */
+var mergeTrees = function (root1, root2) {
+  if (!root1) {
+      return root2
+  } else if (!root2) {
+      return root1
+  } else {
+      return new TreeNode(
+          root1.val + root2.val,
+          mergeTrees(root1.left, root2.left),
+          mergeTrees(root1.right, root2.right)
+      )
+  }
+};
