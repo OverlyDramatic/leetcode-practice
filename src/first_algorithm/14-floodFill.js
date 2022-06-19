69654c5823bb4edd67be4916207d61472b41ba6a
overlyDramatic <maxjunedown@live.com>
2022-06-19 10:07:36 +0800

    feat: bfs for floodFill

--- src/first_algorithm/14-floodFill.js
+++ src/first_algorithm/14-floodFill.js
@@ -8,26 +8,54 @@
  * @return {number[][]}
  */
 var floodFill = function (image, sr, sc, color) {
-  const currentColor = image[sr][sc]
-  if (currentColor === color) {
-    return image
-  }
+  // * dfs
+  // const currentColor = image[sr][sc]
+  // if (currentColor === color) {
+  //   return image
+  // }
 
+  // const m = image.length
+  // const n = image[0].length
+
+  // dfs(sr, sc)
+  // return image
+
+  // function dfs(r, c) {
+  //   if (image[r][c] === currentColor) {
+  //     image[r][c] = color
+  //     r >= 1 && dfs(r - 1, c)
+  //     r + 1 < m && dfs(r + 1, c)
+  //     c >= 1 && dfs(r, c - 1)
+  //     c + 1 < n && dfs(r, c + 1)
+  //   }
+  // }
+  // * bfs
+  const taskQueue = []
+  // const nodeHash = {}
   const m = image.length
   const n = image[0].length
-
-  dfs(sr, sc)
-  return image
-
-  function dfs(r, c) {
-    if (image[r][c] === currentColor) {
+  const originColor = image[sr][sc]
+  if (originColor === color) {
+    return image
+  }
+  taskQueue.push([sr, sc])
+  // nodeHash[`r${sr}c${sc}`] = true
+  while (taskQueue.length) {
+    const [r, c] = taskQueue.shift()
+    const nodeColor = image[r][c]
+    if (nodeColor === originColor) {
       image[r][c] = color
-      r >= 1 && dfs(r - 1, c)
-      r + 1 < m && dfs(r + 1, c)
-      c >= 1 && dfs(r, c - 1)
-      c + 1 < n && dfs(r, c + 1)
+      // r >= 1 && !nodeHash[`r${r - 1}c${c}`] && taskQueue.push([r - 1, c])
+      // r + 1 < m && !nodeHash[`r${r + 1}c${c}`] && taskQueue.push([r + 1, c])
+      // c >= 1 && !nodeHash[`r${r}c${c - 1}`] && taskQueue.push([r, c - 1])
+      // c + 1 < n && !nodeHash[`r${r}c${c + 1}`] && taskQueue.push([r, c + 1])
+      r >= 1 && taskQueue.push([r - 1, c])
+      r + 1 < m && taskQueue.push([r + 1, c])
+      c >= 1 && taskQueue.push([r, c - 1])
+      c + 1 < n && taskQueue.push([r, c + 1])
     }
   }
+  return image
 }
 
 module.exports = floodFill
